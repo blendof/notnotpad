8d8bf4f4271aae2e97181315c374176d2a3bfaae
blendof <44081440+blendof@users.noreply.github.com>
2018-10-12 14:26:07 +0700

    commit two

--- app.js
+++ app.js
@@ -1,40 +1,57 @@
-let val;
+// Define UI Vars
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
 
-val = document;
-val = document.all;
-val = document.all[2];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
-val = document.characterSet;
-val = document.contentType;
+// Load all event listeners
+loadEventListeners();
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
-val = document.forms[0].action;
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener("submit", addTask);
+  //remove task event
+  taskList.addEventListener("click", removeTask);
+}
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
+// Add Task
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Please fill & Add a task");
+    return;
+  }
 
-val = document.images;
+  // Create li element
+  const li = document.createElement("li");
+  // Add class
+  li.className = "collection-item";
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement("a");
+  // Add class
+  link.className = "delete-item secondary-content";
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
 
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
+  // Append li to ul
+  taskList.appendChild(li);
 
-let scripts = document.scripts;
+  // Clear input
+  taskInput.value = "";
 
-let scriptsArr = Array.from(scripts);
+  e.preventDefault();
+}
 
-scriptsArr.forEach(function(script) {
-  console.log(script.getAttribute('src'));
-});
-
-console.log(val);
+// remove task
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("are you sure ?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
